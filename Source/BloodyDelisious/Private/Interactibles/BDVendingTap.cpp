a728bb273822f64ddeec9bbbedd9d2f0a47d73fa
Turbomorsh <Glord.kod@gmail.com>
2024-11-22 00:19:16 +0300

    VendingMachine, drinks in game

--- Source/BloodyDelisious/Private/Interactibles/BDVendingTap.cpp
+++ Source/BloodyDelisious/Private/Interactibles/BDVendingTap.cpp
@@ -1,3 +1,95 @@
 // Bloody Delisious. All Rights Reserved.
 
 #include "Interactibles/BDVendingTap.h"
+
+#include "Components/BoxComponent.h"
+#include "Interactibles/BDCup.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "PhysicsEngine/ShapeElem.h"
+
+ABDVendingTap::ABDVendingTap()
+{
+    RootScene = CreateDefaultSubobject<USceneComponent>("RootSceneComponent");
+    SetRootComponent(RootScene);
+
+    TapSocket = CreateDefaultSubobject<USceneComponent>("TapSocketComponent");
+    TapSocket->SetupAttachment(GetRootComponent());
+
+    TapCollision = CreateDefaultSubobject<UBoxComponent>("TapBoxCollision");
+    TapCollision->SetupAttachment(GetRootComponent());
+
+    TapMesh = CreateDefaultSubobject<UStaticMeshComponent>("TapStaticMesh");
+    TapMesh->SetupAttachment(TapSocket);
+
+    CupSocket = CreateDefaultSubobject<USceneComponent>("CupScene Component");
+    CupSocket->SetupAttachment(GetRootComponent());
+
+    Curve = CreateDefaultSubobject<UCurveFloat>("CurveFloat");
+
+    BindTimeLine();
+}
+void ABDVendingTap::Interact(TObjectPtr<UObject> Object)
+{
+    Super::Interact(Object);
+
+    if (TObjectPtr<ABDCup> CastedCup = Cast<ABDCup>(Object))
+        if (CastedCup->Type == EDrinkType::Empty)
+        {
+            Timeline.PlayFromStart();
+            TakeCup(CastedCup);
+        }
+}
+
+void ABDVendingTap::BeginPlay()
+{
+    Super::BeginPlay();
+
+    InitialRotator = TapSocket->GetRelativeRotation();
+}
+
+void ABDVendingTap::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    if (Timeline.IsPlaying()) Timeline.TickTimeline(DeltaTime);
+}
+
+void ABDVendingTap::BindTimeLine()
+{
+    Curve->FloatCurve.AddKey(0, 0);
+    Curve->FloatCurve.AddKey(0.5, 1);
+
+    Timeline = FTimeline{};
+    FOnTimelineFloat ProgressFunction{};
+    ProgressFunction.BindUFunction(this, "TimeLineUpdate");
+    Timeline.AddInterpFloat(Curve, ProgressFunction, "Float1", "FloatTrack");
+
+    Timeline.SetTimelineLengthMode(ETimelineLengthMode::TL_LastKeyFrame);
+}
+
+void ABDVendingTap::TimeLineUpdate(float Alpha)
+{
+    int StartRot = InitialRotator.Pitch;
+
+    int EndRot = InitialRotator.Pitch - 40;
+
+    TapSocket->SetRelativeRotation(FRotator(
+        UKismetMathLibrary::Lerp(StartRot, EndRot, Alpha), TapSocket->GetRelativeRotation().Yaw, TapSocket->GetRelativeRotation().Roll));
+
+    GEngine->AddOnScreenDebugMessage(98, 5, FColor::Cyan, "Played");
+}
+void ABDVendingTap::TakeCup(TObjectPtr<ABDCup> InCup)
+{
+    SetActorEnableCollision(false);
+    CupReference = InCup;
+    InCup->Grab(CupSocket);
+    InCup->Type = DrinkType;
+    InCup->OnGrabbed.AddDynamic(this, &ABDVendingTap::ClearCup);
+    InCup->SetActorEnableCollision(true);
+}
+void ABDVendingTap::ClearCup()
+{
+    SetActorEnableCollision(true);
+    Timeline.Reverse();
+    CupReference = nullptr;
+}
--- Source/BloodyDelisious/Public/Interactibles/BDVendingTap.h
+++ Source/BloodyDelisious/Public/Interactibles/BDVendingTap.h
@@ -3,9 +3,13 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "BDCup.h"
+#include "Components/TimelineComponent.h"
 #include "Interactibles/BDStaticInteractionActor.h"
 #include "BDVendingTap.generated.h"
 
+class ABDCup;
+class UBoxComponent;
 /**
  *
  */
@@ -15,4 +19,51 @@ class BLOODYDELISIOUS_API ABDVendingTap : public ABDStaticInteractionActor
     GENERATED_BODY()
 
 public:
+    ABDVendingTap();
+
+    virtual void Interact(TObjectPtr<UObject> Object) override;
+
+protected:
+    UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = Component)
+    TObjectPtr<UStaticMeshComponent> TapMesh;
+
+    UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = Component)
+    TObjectPtr<USceneComponent> CupSocket;
+
+    UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = Component)
+    TObjectPtr<UBoxComponent> TapCollision;
+
+    UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = Component)
+    TObjectPtr<USceneComponent> RootScene;
+
+    UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = Component)
+    TObjectPtr<USceneComponent> TapSocket;
+
+    UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = States)
+    TEnumAsByte<EDrinkType> DrinkType;
+
+    UPROPERTY()
+    TObjectPtr<ABDCup> CupReference;
+
+    UPROPERTY()
+    FTimeline Timeline;
+
+    UPROPERTY()
+    TObjectPtr<UCurveFloat> Curve;
+
+    FRotator InitialRotator;
+
+    virtual void BeginPlay() override;
+
+    virtual void Tick(float DeltaTime) override;
+
+    void BindTimeLine();
+
+    UFUNCTION()
+    void TimeLineUpdate(float Alpha);
+
+    void TakeCup(TObjectPtr<ABDCup> InCup);
+
+    UFUNCTION()
+    void ClearCup();
 };
