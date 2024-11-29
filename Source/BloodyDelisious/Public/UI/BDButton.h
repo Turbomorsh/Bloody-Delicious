38cde674b6fba1c65a11c824c7ea188fc7568484
Artem Azokhov <azokhov@gmail.com>
2024-11-30 00:24:57 +0400

    Pause menu design

---
+++ Source/BloodyDelisious/Public/UI/BDButton.h
@@ -0,0 +1,13 @@
+// Bloody Delisious. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Button.h"
+#include "BDButton.generated.h"
+
+UCLASS()
+class BLOODYDELISIOUS_API UBDButton : public UButton
+{
+    GENERATED_BODY()
+};
